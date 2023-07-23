2f0524aa5f35da58ddadc3f2af017d7f50ea4c3b
Mohammed Ilyas <71893157+Masterchild@users.noreply.github.com>
2023-07-23 23:35:25 +0530

    image and code fix

--- index.js
+++ index.js
@@ -264,6 +264,6 @@ function openWhatsapp() {
     }
 
     let wTxtEncoded = encodeURI(wTxt);
-    window.open("https://wa.me/917428789065?text=" + wTxtEncoded);
+    window.open("https://wa.me/916385305002?text=" + wTxtEncoded);
   }
 }
